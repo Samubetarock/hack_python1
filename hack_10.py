f87acdb5c5dff6f9928cd7a637b6344622d1eed5
Samubetarock <samuelgonzalez164@gmail.com>
2024-03-06 23:01:21 -0400

    subida de hacks python

--- hack_10.py
+++ hack_10.py
@@ -1,8 +1,20 @@
 """
-text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
+text: "fooziman"  output => ["F","0","0","Z","1","M","@","N"]
 """
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    modified_result = []
+
+    for char in result:
+        if char == 'o':
+            modified_result.extend(['0'])
+        elif char == 'i':
+            modified_result.extend(['1'])
+        elif char == 'a':
+            modified_result.extend(['@'])
+        else:
+            modified_result.extend([char.upper()])
+
+    return modified_result
+
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,9 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
+    i = 1
+    while i <= len(result):
+        result.insert(i, "@")
+        i += 2
     return result  
+
